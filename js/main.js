4bcd93edd9b80055f09d9e199ce7d50acc54f767
Trevor Van Rys <halvas_trevor@halvas.ca>
2015-05-21 12:31:57 -0400

    create site files
    adding assets…

---
+++ js/main.js
@@ -0,0 +1,40 @@
+(function(){
+	console.log('fired!');
+	
+	var textOptions = ["an image", "a video", "an email", "a decision", "a behaviour", "a conversation", "multichannel", "smart content", "DIGITAL ENGAGEMENT"];
+	var heroText = document.querySelector('.animStrong'),
+		tlAnim = new TimelineLite(),
+		counter = 0,
+		timer;
+
+	function animateText() {
+		console.log('animateText fired');
+		tlAnim.to(heroText, .7, {opacity: 0, onComplete:switchText})
+		.to(heroText, .7, {opacity: 1});
+	}
+
+	function switchText() {
+		
+		if (counter == textOptions.length -1) {
+			//counter = 0;
+			resetAnim();
+		}
+		heroText.innerHTML = textOptions[counter];
+
+		counter++;
+	}
+
+	function resetAnim() {
+		console.log('resetAnim fired');
+		clearInterval(timer);
+		timer = null;
+
+		setTimeout(function() {
+			counter = 0;
+			//tlAnim.stop();
+			timer = setInterval(animateText, 2500); }, 6000 );
+	}
+
+	animateText();
+	timer = setInterval(animateText, 2500);
+})();
